24ca61057dfcca914271ff9c377f66e74e401b05
Eduardo Venegas <lalohdez77@gmail.com>
2020-11-16 09:09:42 -0600

    commit before firebase :fire:

--- App/src/app/tab1/tab1.page.ts
+++ App/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,47 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  
+  @ViewChild('barChart') barChart;
+
+  bars: any;
+  colorArray: any;
   constructor() {}
+
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['1:00', '2:00', '3:00', '4:00', '5:00', '6:00', '7:00', '8:00'],
+        datasets: [{
+          label: 'Temperature',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgba(255, 43, 43, 0.2)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(255, 43, 43)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        },
+        {
+          label: 'Humidity',
+          data: [35, 40, 56, 48, 47, 52, 38, 25],
+          backgroundColor: 'rgba(56, 128, 255,0.2)', // array should have same number of elements as number of dataset
+          borderColor: 'rgba(56, 128, 255,1)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
 
 }
--- App/src/app/tab2/tab2.page.ts
+++ App/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {formatDate} from '@angular/common';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,16 +9,80 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  light = false;
-  fan = false;
+  
+  myDate: any = new Date();
+  constructor() {}
+
+  weatherIcon: string;
+
+  precProb: number = 18;
+  
+  armed: boolean = false;
+  armedText: string;
+  armedIcon: string;
+  armedColor: string;
+  
+  door: boolean = false;
+  doorText: string;
+  doorColor: string;
+  
+  window: boolean = false;
+  windowText: string;
+  windowColor: string;
+  
+  light: boolean = false;
+  lightIntensity: number = 0;
+  lightColor: string;
+
+  fan: boolean = false;
+  fanSpeed: number = 0;
+  fanColor: string;
+  
+  ngOnInit() {
+    console.log(formatDate(new Date(), 'dd', 'en'))
+    this.precProb >= 50 ? this.weatherIcon = "rainy-outline" : this.weatherIcon = "sunny-outline" ;
+
+    this.armed ?
+      (this.armedText = "Armed", this.armedIcon = "lock-closed-outline",  this.armedColor="danger" ) :
+      (this.armedText = "Disarmed", this.armedIcon="lock-open-outline", this.armedColor="");
+
+    this.door ? (this.doorText = "Door closed", this.doorColor="tertiary") : (this.doorText = "Door opened", this.doorColor="");
+    this.window ? (this.windowText = "Window closed", this.windowColor="success") : (this.windowText = "Window opened", this.windowColor="");
+    this.light ? (this.lightIntensity = 255, this.lightColor="warning") : (this.lightIntensity = 0, this.lightColor="");
+  }
+
+  onArmed() {
+    this.armed = !this.armed;
+    this.armed ?
+      (this.armedText = "Armed", this.armedIcon = "lock-closed-outline", this.armedColor="danger" ) :
+      (this.armedText = "Disarmed", this.armedIcon="lock-open-outline", this.armedColor="");
+  }
+  onDoor() {
+    this.door = !this.door;
+    this.door ? (this.doorText = "Door closed", this.doorColor="tertiary") : (this.doorText = "Door opened", this.doorColor="");
+  }
+  onWindow() {
+    this.window = !this.window;
+    this.window ? (this.windowText = "Window closed", this.windowColor="success") : (this.windowText = "Window opened", this.windowColor="");
+  }
 
   onLight() {
     this.light = !this.light;
+    this.light ? (this.lightIntensity = 255, this.lightColor="warning") : (this.lightIntensity = 0, this.lightColor="");
   }
   onFan() {
     this.fan = !this.fan;
+    this.fan ? (this.fanSpeed = 255, this.fanColor="secondary") : (this.fanSpeed = 0, this.fanColor="");
   }
 
-  constructor() {}
+
+  onLightChange(e: any) {
+    this.lightIntensity = e.detail.value;
+    console.log("Light: ", this.lightIntensity);
+  }
+  onFanChange(e: any) {
+    this.fanSpeed = e.detail.value;
+    console.log("Fan:   ", this.fanSpeed);
+  }
 
 }
